a6f0d56c0bc4fa98ee8ab51b2b3ee2c87ca5dfdc
Edgar Ocampo <davidocampoc11@gmail.com>
2026-01-13 12:55:46 -0500

    Config: ajustes de conexión y settings

--- Shared.Data/DatabaseConfig.cs
+++ Shared.Data/DatabaseConfig.cs
@@ -4,5 +4,5 @@ public static class DatabaseConfig
 {
     // Cadena de conexión a la base de datos SQL Server en Somee.com
     // NOTA: En producción (Railway), esto será sobrescrito por variables de entorno
-    public const string ConnectionString = "Server=db31651.public.databaseasp.net;Database=db31651;User Id=db31651;Password=prueba2020d;Encrypt=True;TrustServerCertificate=True;MultipleActiveResultSets=True;";
+    public const string ConnectionString = "Server=db32030.public.databaseasp.net;Database=db32030;User Id=db32030;Password=3s%K-7Hxn_9E;Encrypt=True;TrustServerCertificate=True;MultipleActiveResultSets=True;";
 }
